9736b03ee776117e02b74d0f9876d8d3d2105b30
Mohini-Sakhare <sakharemohini123@gmail.com>
2025-07-16 23:31:25 +0530

    create onload function in js file

---
+++ Game.js
@@ -0,0 +1,9 @@
+onload = function(){
+    let parent = document.getElementById("drag");
+    let frag = document.createDocumentFragment();
+    while(parent.children.length)
+    {
+        frag.appendChild(parent.children[Math.floor(Math.random() * parent.children.length)]);
+    }
+    parent.appendChild(frag);
+}
